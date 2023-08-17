a5cde0c86ee21518ef9b0fd5699569610cef3b36
DavidGMont <davidgarmo@outlook.com>
2023-08-16 20:57:00 -0500

    😵 Tarjeta casi terminada

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
+import styles from './Card.module.css';
 
 const Card = ({ cardDetails }) => {
     return (
-        <>
+        <div className={styles.card}>
             <h2>{cardDetails.cardNumber}</h2>
             <h3>{cardDetails.name}</h3>
             <p>
                 {cardDetails.month}/{cardDetails.year}
             </p>
             <p>{cardDetails.cvc}</p>
-        </>
+        </div>
     );
 };
 
